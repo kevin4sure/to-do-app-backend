50177b9514d223ba60ed163ca014518cc19e9b68
Kamlesh Gupta <kamleshguptaslg@gmail.com>
2021-01-02 19:05:38 +0530

    minor chnages

--- TasksApp/serializers.py
+++ TasksApp/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import Task, Bucket
 
 class TaskSerializer(serializers.ModelSerializer):
-    bucket_id = serializers.CharField(write_only=True,required=False)
+    bucket_id = serializers.CharField(write_only=True,required=True)
     bucket = serializers.SerializerMethodField()
     class Meta:
         model = Task
@@ -17,23 +17,14 @@ class TaskSerializer(serializers.ModelSerializer):
             return None 
 
     def create(self, validated_data):
-        bucket_id = None 
-        bucket, _ = Bucket.objects.get_or_create(name='default')
-        try: 
-            bucket_id = validated_data.pop('bucket')
-            bucket = Bucket.objects.get(id=bucket_id)
-        except IndexError: pass
-        except KeyError: pass
-        except Bucket.DoesNotExist: pass
-
-        new_task = Task.objects.create(bucket=bucket, **validated_data)
+        new_task = Task.objects.create(**validated_data)
         return new_task
 
     def update(self,obj ,validated_data, *args, **kwargs):
         # try: validated_data.pop('validated_data')
         # except IndexError: pass
         # except KeyError: pass
-        return super().update(obj,validated_data,*args, **kwargs)        
+        return super().update(obj,validated_data,*args, **kwargs) 
 
 
 class BucketSerializer(serializers.ModelSerializer):
